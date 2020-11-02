946da065d2e09aba2873415151eb5c8a2c59ab11
17244 <WEI.LI14@HAND-CHINA.COM>
2020-11-02 10:01:38 +0800

    this is new commit please update frist

--- README.md
+++
@@ -1,3 +0,0 @@
-# SpringCloudTem
-Spring
- 2020/10/21 该项目用于做springcloud模板微服务基础搭建。
--- spring_cloud_eureka/src/test/java/com/liwei/SpringCloudEurekaApplicationTests.java
+++ spring_cloud_eureka/src/test/java/com/liwei/SpringCloudEurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com;
+package com.liwei;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringeurekaApplicationTests {
+class SpringCloudEurekaApplicationTests {
 
 	@Test
 	void contextLoads() {
